bee804a8c14f07e8624a4220d28278dd789cd5a8
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-07-26 22:20:07 +0700

    props

--- src/Component/Fungsional/Home.jsx
+++ src/Component/Fungsional/Home.jsx
@@ -3,10 +3,23 @@ import React from 'react'
 import Produk from '../Class/Produk';
 
 const Home = () => {
+    return <div><h1>Ini adalah Halaman Utama</h1></div>
     return <div>
-    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
-    <Produk nama="Xiaomi Pro 8" stock="10" harga="3.000.000"/>
-    
+        <Blog
+            tanggal="15 juni 2020"
+            judul=" Teknologi Blockchain"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog
+            tanggal="16 juni 2020"
+            judul=" Teknologi Internet of Things"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
+        <Blog
+            tanggal="17 juni 2020"
+            judul=" Teknologi Blockchain"
+            summary=" Generate Lorem Ipsum placeholder text for use in your graphic"
+        />
     </div>
 }
 
