8447f9a301cd1c53b17199b98d374d057f10abde
wahyuekamulatsari <wahyuekamalik@gmail.com>
2020-07-27 08:52:31 +0700

    create data admin

---
+++ src/Component/Hooks/Functional/LoginAdmin.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const loginadmin = () => {
+    <div>
+    
+                                    <label >Password</label>
+                                    <input type="password" className="form-control"  placeholder="Masukan Password" />
+                                </div>
+                                    <button type="submit" class="btn btn-primary btn-lg btn-block">M A S U K</button>   
+                                    <Link to="/useeffects"button type="submit" class="btn btn-primary btn-lg btn-block">M A S U K</Link>   
+                            </form>
+                        </div>
+                </div>
+                </div>
+         );
+    }
+export default loginadmin ;
